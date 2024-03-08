643bccb5107a6ac2326a53cbae93b468256e0649
Bartosz Jakubski <46166114+Proallone@users.noreply.github.com>
2024-03-08 08:20:26 +0100

    Code refactor

--- backuper.py
+++ backuper.py
@@ -1,43 +1,69 @@
 import os
-import sys
-import shutil 
-from datetime import date, datetime, timedelta
+import shutil
+from datetime import date, datetime
 
-ROOT = os.path.abspath( os.path.dirname( __file__ ) )
-os.chdir(ROOT)
 
-CP_SAVES_FOLDER = './Cyberpunk 2077'
-CP_BACKUPS_FOLDERS = sorted(next(os.walk("./"))[1], key=os.path.getmtime, reverse=True)
+def backup():
+    """
+    Does the backup.
 
-LAST_SAVE_MTIME = os.stat(CP_SAVES_FOLDER).st_mtime
-LAST_BACKUP_CTIME = os.stat(CP_BACKUPS_FOLDERS[0]).st_ctime
-
-TODAY = date.today().strftime('%d-%m-%Y')
-BACKUP_DESTINATION = f'AutoBackup-{TODAY}'
-
-def make_backup():
+    Returns:
+        boolean: True if backup was successfull. False otherwise.
+    """
+    TODAY = date.today().strftime('%d-%m-%Y')
+    CP_SAVES_FOLDER = './Cyberpunk 2077'
+    BACKUP_DESTINATION = f'AutoBackup-{TODAY}'
     try:
-        if(is_backup_needed()):
+        walk_to_root()
+        LAST_SAVE_MTIME = os.stat(CP_SAVES_FOLDER).st_mtime
+        CP_BACKUPS_FOLDERS = sort_backups()
+        LAST_BACKUP_CTIME = os.stat(CP_BACKUPS_FOLDERS[0]).st_ctime
+        if (is_backup_needed(CP_SAVES_FOLDER, CP_BACKUPS_FOLDERS)):
             shutil.copytree(CP_SAVES_FOLDER, BACKUP_DESTINATION)
             print(f'Cyberpunk {TODAY} saves backup successfull!')
         else:
             print(f'No new saves, no backup needed.\nNewest save date: {datetime.fromtimestamp(LAST_SAVE_MTIME)}.\nNewest backup date: {datetime.fromtimestamp(LAST_BACKUP_CTIME)}')
     except OSError as error:
-        print(error) 
-    
-def is_backup_needed ():
-    if(LAST_SAVE_MTIME > LAST_BACKUP_CTIME):
-        return True
-    else:
-        return False
+        print(error.strerror)
 
-def remove_old_backups(older_than_in_days=7):
+
+def sort_backups():
+    """
+    Sorts existing backup folders by creation date in descending order. 
+
+    Returns:
+        array: Sorted array of saves folders in the Cyberpunk 2077 folder.
+    """
     try:
-        shutil.rmtree(f'./AutoBackup-{TODAY}')
+        return sorted(next(os.walk("./"))[1], key=os.path.getmtime, reverse=True)
     except OSError as error:
-        print(error)
-    
+        raise error
 
-make_backup()
-# print ('Number of arguments:', str(sys.argv), 'arguments')
-# print(date.today() - timedelta(days=7))
+
+def walk_to_root():
+    """
+    Sets working directory context to the Cyberpunk 2077 saves folder (if backuper.py was placed correctly)
+    """
+    os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+def is_backup_needed(CP_SAVES_FOLDER, CP_BACKUPS_FOLDERS):
+    """
+    Compares last save modification date with most recent backup date.
+
+    Returns:
+        boolean: True if there is any new save to backup. False otherwise.
+    """
+    try:
+        return os.stat(CP_SAVES_FOLDER).st_mtime > os.stat(CP_BACKUPS_FOLDERS[0]).st_ctime
+    except OSError as error:
+        raise error
+
+# todo - add optional removal of old backups within given interval
+# def remove_old_backups(TODAY,older_than_in_days=7):
+#     try:
+#         shutil.rmtree(f'./AutoBackup-{TODAY}')
+#     except OSError as error:
+#         raise error
+
+
+backup()
